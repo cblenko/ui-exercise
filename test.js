48acb00f915756e4240d3bb1ab24125fe8f18a68
Carl Blenkinsop <carl@blenko.co.uk>
2017-10-20 16:32:34 +0100

    Exercise 3

--- test.js
+++ test.js
@@ -2,4 +2,21 @@ $(document).on('ready', function bootstrap() {
     $('#nav-toggle').on('click', function toggle(e) {
         $('#nav-items').toggleClass('show');
     })
+
+    // implemted this way due to lack of transition in <IE10
+    $('#side-bar').on('click', function toggle(e) {
+        var main = $('main'),
+            sideBar = $('main > div.sidebar');
+
+        if(main.attr('data-sidebar')){
+            main.animate({ paddingRight: 0 });
+            sideBar.animate({ left: '100%' });
+            main.removeAttr('data-sidebar');
+        } else {
+            main.animate({ paddingRight: '30%' });
+            sideBar.animate({ left: '70%' });
+            main.attr('data-sidebar', true);
+        }
+        
+    })
 });
